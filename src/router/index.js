0ab0592498195e2e2c949b3084e7ccf145d49d00
sauqy <asauqy11@gmail.com>
2022-07-02 21:26:07 +0700

    create logins and regis page

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import Pulsa from "../views/Pulsa.vue";
 import ListPulsa from "../views/ListPulsa.vue";
 import EMoney from "../views/EMoney.vue";
 import CashOut from "../views/CashOut.vue";
+import LoginAdmin from "../views/LoginAdmin.vue";
+import Register from "../views/Register.vue";
 
 Vue.use(VueRouter);
 
@@ -46,8 +48,17 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    // component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/logins",
+    name: "logins",
+    component: LoginAdmin,
+  },
+  {
+    path: "/regis",
+    name: "regis",
+    component: Register,
   },
 ];
 
---
+++ src/views/Register.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field v-model="email"> </v-text-field>
+    </v-form>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field v-model="password"> </v-text-field>
+    </v-form>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field v-model="notlp"> </v-text-field>
+    </v-form>
+    <v-btn color="success" class="mr-4">Registrasi </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "regis",
+};
+</script>
